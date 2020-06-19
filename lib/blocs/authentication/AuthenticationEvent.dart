ab30ac6a556ea460000e8219dfb0d1b7351ed1a5
paaweel <pawel.gakan@gmail.com>
2020-06-19 19:21:17 +0200

    Add firebase example

---
+++ lib/blocs/authentication/AuthenticationEvent.dart
@@ -0,0 +1,44 @@
+part of 'AuthenticationBloc.dart';
+
+@immutable
+abstract class AuthenticationEvent extends Equatable {
+  AuthenticationEvent([List props = const <dynamic>[]]) : super(props);
+}
+
+class AppLaunched extends AuthenticationEvent {
+  @override
+  String toString() => 'AppLaunched';
+}
+
+class ClickedGoogleLogin extends AuthenticationEvent {
+  @override
+  String toString() => 'ClickedGoogleLogin';
+}
+
+class LoggedIn extends AuthenticationEvent {
+  final FirebaseUser user;
+  LoggedIn(this.user);
+  @override
+  String toString() => 'LoggedIn';
+}
+
+class PickedProfilePicture extends AuthenticationEvent{
+  final File file;
+  PickedProfilePicture(this.file);
+  @override
+  String toString() => 'PickedProfilePicture';
+}
+
+class SaveProfile extends AuthenticationEvent {
+  final File profileImage;
+  final int age;
+  final String username;
+  SaveProfile(this.profileImage, this.age, this.username);
+  @override
+  String toString() => 'SaveProfile';
+}
+
+class ClickedLogout extends AuthenticationEvent {
+  @override
+  String toString() => 'ClickedLogout';
+}
