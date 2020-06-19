727b3435b69d6c6af58a09438ccae6843b4aca45
paaweel <pawel.gakan@gmail.com>
2020-06-19 18:51:28 +0200

    Add firebase example

--- lib/blocks/authentication/bloc/AuthenticationEvent.dart
+++
@@ -1,5 +0,0 @@
-part of 'AuthenticationBloc.dart';
-
-abstract class AuthenticationEvent extends Equatable {
-  const AuthenticationEvent();
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
 import 'config/Palette.dart';
-// import 'pages/ConversationPageSlide.dart';
 import 'pages/RegisterPage.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kopper/repositiories/AuthenticationRepository.dart';
+import 'package:kopper/repositiories/StorageRepository.dart';
+import 'package:kopper/repositiories/UserDataRepository.dart';
+import 'package:kopper/blocs/authentication/AuthenticationBloc.dart';
+import 'package:kopper/pages/ConversationPageSlide.dart';
 
-void main() => runApp(Kopper());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  //create instances of the repositories to supply them to the app
+  final AuthenticationRepository authRepository = AuthenticationRepository();
+  final UserDataRepository userDataRepository = UserDataRepository();
+  final StorageRepository storageRepository = StorageRepository();
+  runApp(
+    BlocProvider(
+      create: (context) => AuthenticationBloc(
+          authenticationRepository: authRepository,
+          userDataRepository: userDataRepository,
+          storageRepository: storageRepository)
+        ..add(AppLaunched()),
+      child: Kopper(),
+    ),
+  );
+}
 
 class Kopper extends StatelessWidget {
   @override
@@ -14,7 +35,18 @@ class Kopper extends StatelessWidget {
         primaryColor: Palette.primaryColor,
 //        primarySwatch: Palette.primaryColor,
       ),
-      home: RegisterPage()//ConversationPageSlide(),
+      // home: RegisterPage()
+      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+        builder: (context, state) {
+          if (state is UnAuthenticated) {
+            return RegisterPage();
+          } else if (state is ProfileUpdated) {
+            return ConversationPageSlide();
+          } else {
+            return RegisterPage();
+          }
+        },
+      ),
     );
   }
 }
